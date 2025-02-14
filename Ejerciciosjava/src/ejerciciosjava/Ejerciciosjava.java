c7bd7d127d9dadff99798c9e912bfcd809469e05
taylorbernat <taylorfabianbernate@gmail.com>
2025-02-14 09:29:07 -0500

    Add files via upload

---
+++ Ejerciciosjava/src/ejerciciosjava/Ejerciciosjava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author SENA
+ */
+public class Ejerciciosjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio1 trabajo1 =new Ejercicio1();
+        trabajo1.factura();
+        // TODO code application logic here
+    }
+    
+}
